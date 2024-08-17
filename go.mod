9e93f3c55fbb390258652f922095c2991e981d4b
Chris Cadden <cadden.cs@gmail.com>
2024-08-17 17:42:30 -0400

    feat: compiles and runs, but always takes max execution
    Thinking it's some problem in how we're tracking the state of the
    discards and deck being empty

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ccadden/war-cardgame
 
 go 1.22.2
 
-require github.com/ccadden/beat-the-box v0.0.0-20240731232052-05c63d4c816f // indirect
+require github.com/ccadden/beat-the-box v0.0.0-20240802130916-0780a04ec040
--- main.go
+++ main.go
@@ -5,6 +5,181 @@ import (
 	"github.com/ccadden/beat-the-box/deck"
 )
 
+type Player struct {
+	deck     *deck.Deck
+	discards *deck.Deck
+}
+
 func main() {
-	fmt.Println("War, what is it good for? Passing a rainy afternoon. SAY IT AGAIN.")
+	roundCounter := 0
+
+	p1 := newPlayer()
+	p2 := newPlayer()
+
+	for (!p1.hasLost() || !p2.hasLost()) && roundCounter < 10000 {
+		if p1.deck.Empty() {
+			p1.reshuffle()
+		}
+
+		if p2.deck.Empty() {
+			p2.reshuffle()
+		}
+
+		c1, ok := p1.dealCard()
+
+		if !ok {
+			panic("Couldn't deal card")
+		}
+
+		c2, ok := p2.dealCard()
+
+		if !ok {
+			panic("Couldn't deal card")
+		}
+
+		switch {
+		case c1 > c2:
+			p1.discards.AddCards([]int{c1, c2})
+		case c2 > c1:
+			p2.discards.AddCards([]int{c1, c2})
+		default:
+			declareWar(p1, p2)
+		}
+
+		roundCounter++
+	}
+
+	fmt.Printf("Took %v rounds\n", roundCounter)
+}
+
+func newPlayer() *Player {
+	p := Player{}
+	p.deck = deck.NewDeck()
+	p.discards = deck.NewDeck()
+
+	return &p
+}
+
+func (p *Player) hasLost() bool {
+	return p.deck.Empty() && p.discards.Empty()
+}
+
+func (p *Player) reshuffle() {
+	p.deck = p.discards
+	p.discards = deck.NewDeck()
+}
+
+func (p *Player) totalCards() int {
+	return p.deck.CardsRemaining() + p.discards.CardsRemaining()
+}
+
+func (p *Player) numCardsToWager() int {
+	if p.totalCards() < 4 {
+		return 0
+	}
+
+	return 3
+}
+
+func (p *Player) dealCard() (int, bool) {
+	if p.totalCards() < 1 {
+		return 0, false
+	}
+
+	if p.deck.CardsRemaining() == 0 {
+		p.reshuffle()
+	}
+
+	deadCard, ok := p.deck.Deal()
+
+	if !ok {
+		return 0, false
+	}
+
+	return deadCard, true
+}
+
+func (p *Player) dealMultiple(numCards int) ([]int, bool) {
+	numCardsToDeal := min(numCards, p.totalCards())
+	cardsToReturn := []int{}
+
+	for range numCardsToDeal {
+		cardToAdd, ok := p.dealCard()
+		if !ok {
+			return []int{}, false
+		}
+
+		cardsToReturn = append(cardsToReturn, cardToAdd)
+	}
+
+	return cardsToReturn, true
+}
+
+func declareWar(p1, p2 *Player) bool {
+	numCards1 := p1.numCardsToWager()
+	numCards2 := p2.numCardsToWager()
+
+	if numCards1 == 0 || numCards2 == 0 {
+		// Someone is about to lose anyway, not sure we can even get here
+		return true
+	}
+
+	wageredCards1, ok := p1.dealMultiple(numCards1)
+
+	if !ok {
+		panic("problem dealing multiple cards for p1")
+	}
+
+	wageredCards2, ok := p1.dealMultiple(numCards2)
+
+	if !ok {
+		panic("problem dealing multiple cards for p1")
+	}
+
+	if !ok {
+		return false
+	}
+
+	c1, ok := p1.dealCard()
+	c2, ok := p2.dealCard()
+
+	switch {
+	case c1 > c2:
+		p1.deck.AddCards([]int{c1, c2})
+		p1.deck.AddCards(wageredCards1)
+		p1.deck.AddCards(wageredCards2)
+	case c2 > c1:
+		p2.deck.AddCards([]int{c1, c2})
+		p2.deck.AddCards(wageredCards1)
+		p2.deck.AddCards(wageredCards2)
+	default:
+		declareWar(p1, p2)
+	}
+
+	return true
+}
+
+func (p *Player) numWarCardsToWager() int {
+	totalCards := p.totalCards()
+
+	if totalCards == 0 {
+		return 0
+	}
+
+	cardsToWager := min(totalCards, 3)
+
+	if totalCards <= 3 {
+		// we need to have a card to actually flip in the war
+		cardsToWager -= 1
+	}
+
+	return cardsToWager
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
 }
